70f0afb819037723a405ede0bd032f5908182849
Letl1294 <40808643+Letl1294@users.noreply.github.com>
2018-07-10 22:17:02 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,28 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var meses = document.getElementById('mes').value;
+ 
+switch (meses) 
+{
+    case "Enero": 
+    alert("Buen año");
+    break;
+    
+    case "Marzo":
+    alert("Clases");
+    break;
 
+    case "Julio":
+    alert("Vacaciones");
+    break;
 
+    case "Diciembre":
+
+    alert("Fiestas");
+    break;
+
+}
 
 
 }//FIN DE LA FUNCIÓN
