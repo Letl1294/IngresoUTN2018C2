eb157b3d52acade15d19eb8be8c05229cef424f7
Letl1294 <40808643+Letl1294@users.noreply.github.com>
2018-07-11 22:18:28 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+var num1,num2,resul;
+
+num1 = prompt("Ingrese un numero");
+num2 = prompt("Ingrese otro numero");
+
+if(num1==num2)
+{
+alert(num1+num2);
+}
+else if(parseInt(num1)>parseInt(num2))
+{
+alert(num1-num2);
+}
+else
+{
+resul = parseInt(num1)+parseInt(num2); 
+    if(resul>10)
+     {
+        alert("La suma es "+resul+" y supero a 10");
+     }
+    else
+     {
+        alert("La suma es "+resul);
+     }
+
+}
 
 }
