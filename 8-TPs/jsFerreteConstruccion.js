f405d75e6596ab8eed5f8419510f7030240584ac
Letl1294 <40808643+Letl1294@users.noreply.github.com>
2018-07-29 16:15:51 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -3,16 +3,48 @@ A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el anch
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
+var ancho, largo, radio;
+var alambre, circulo;
+var bolsas= 2
+var cal= 3;
+
+
+
 
 function Rectangulo () 
 {
-
+largo = parseInt(document.getElementById("Largo").value);
+ancho = parseInt(document.getElementById("Ancho").value);
+alambre = (ancho*largo)*3;
+alert("cantidad de alambre en m "+alambre);
 }
 function Circulo () 
 {
-	
+radio = parseInt(document.getElementById("Radio").value);
+circulo = (3.14*(radio*radio))*3;
+alert("cantidad de alambre en m "+circulo);
 }
 function Materiales () 
 {
-	
+largo = parseInt(document.getElementById("Largo").value);
+ancho = parseInt(document.getElementById("Ancho").value);
+
+    if(ancho==1 && largo==1){
+        bolsas = 2;
+        cal = 3;
+    }
+    if(ancho>1 && largo==1){
+            bolsas = (ancho*0.5)+2;
+            cal = (ancho*0.75)+3;
+     }   
+    if(ancho==1 && largo>1){
+            bolsas = (largo*0.5)+2;
+            cal = (largo*0.75)+3;
+    }
+    if(largo>1 && ancho>1){
+            bolsas = largo*2;
+            cal = largo*3;
+    }    
+       alert("bolsas de cemento "+ bolsas + " bolsas de cal "+cal);
 }
+    
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,27 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+var numero1;
+var numero2;
+var numero3;
+var suma;
+var promedio;
+var precioFinal;
 function Sumar () 
 {
-	
+numero1 = parseInt(document.getElementById("PrecioUno").value);
+numero2 = parseInt(document.getElementById("PrecioDos").value);
+numero3 = parseInt(document.getElementById("PrecioTres").value);
+suma = numero1+numero2+numero3;
+alert(suma);
 }
 function Promedio () 
 {
-	
+promedio = suma/3,
+alert(promedio);
 }
 function PrecioFinal () 
 {
-	
+precioFinal = suma*1.21;
+alert(precioFinal);
 }
